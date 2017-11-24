f22e28219e13efc93a34dd5d545d7c2a198a219a
panda <84305510@qq.com>
2017-11-24 11:43:39 +0800

    判断用户是否登录

--- case/login.py
+++ case/login.py
@@ -6,35 +6,63 @@ from iphoneinfo import shoujiinfo
 device=shoujiinfo.connnect_ipad_device()
 import time
 
-def login():
 
+def ture_or_flase():
+
+	try:
+		a=device.find_elements_by_class_name ('android.widget.TextView') [14]
+		print(a)
+
+		return True
+
+	except Exception as e:
+
+		print(e)
+		return False
+def login():
+	print(ture_or_flase())
 	# 等待启动，设置程序休眠/防止手机反应慢卡死
 	time.sleep(5)
 	#点击账户
 	device.find_elements_by_class_name ('android.widget.RadioButton') [3].click ()
 	#点击更多
-	time.sleep(2)
-	device.find_elements_by_class_name('android.widget.TextView')[14].click()
-	#退出
-	time.sleep(2)
-	device.find_elements_by_class_name('android.widget.Button')[0].click()
-	#确定
-	time.sleep(2)
-	device.find_elements_by_class_name('android.widget.Button')[0].click()
-	# 点击账户
-	device.find_elements_by_class_name ('android.widget.RadioButton') [3].click ()
-	#登录
-	device.find_elements_by_class_name('android.view.View')[6].click()
-	#登录账户
-	device.find_elements_by_class_name ('android.widget.EditText') [0].clear()
-	login_ip=device.find_elements_by_class_name('android.widget.EditText')[0]
-	device.set_value(login_ip,'18519291259')
-	device.find_elements_by_class_name('android.widget.Button')[0].click()
-	#密码
-	password=device.find_elements_by_class_name('android.widget.EditText')[0]
-	device.set_value(password,'111111')
-	device.find_elements_by_class_name('android.widget.Button')[0].click()
-	#跳过手势密码
-	device.find_elements_by_class_name('android.widget.ImageView')[9].click()
+	if ture_or_flase() is True:
+		device.find_elements_by_class_name ('android.widget.TextView') [14].click()
+		device.find_elements_by_class_name('android.widget.Button')[0].click()
+		#确定
+
+		device.find_elements_by_class_name('android.widget.Button')[0].click()
+		# 点击账户
+		device.find_elements_by_class_name ('android.widget.RadioButton') [3].click ()
+		#登录
+		device.find_elements_by_class_name('android.view.View')[6].click()
+		#登录账户
+		device.find_elements_by_class_name ('android.widget.EditText') [0].clear()
+		login_ip=device.find_elements_by_class_name('android.widget.EditText')[0]
+		device.set_value(login_ip,'18519291259')
+		device.find_elements_by_class_name('android.widget.Button')[0].click()
+		#密码
+		password=device.find_elements_by_class_name('android.widget.EditText')[0]
+		device.set_value(password,'111111')
+		device.find_elements_by_class_name('android.widget.Button')[0].click()
+		#跳过手势密码
+		device.find_elements_by_class_name ('android.widget.ImageView') [9].click ()
+	else:
+		# 登录
+		device.find_elements_by_class_name ('android.view.View') [6].click ()
+		# 登录账户
+		device.find_elements_by_class_name ('android.widget.EditText') [0].clear ()
+		login_ip = device.find_elements_by_class_name ('android.widget.EditText') [0]
+		device.set_value (login_ip, '18519291259')
+		device.find_elements_by_class_name ('android.widget.Button') [0].click ()
+		# 密码
+		password = device.find_elements_by_class_name ('android.widget.EditText') [0]
+		device.set_value (password, '111111')
+		device.find_elements_by_class_name ('android.widget.Button') [0].click ()
+		# 跳过手势密码
+		device.find_elements_by_class_name ('android.widget.ImageView') [9].click ()
+
+
 if __name__ == '__main__':
-    login()
+
+	login()
--- case/shuaxin.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Author  : panda
-from iphoneinfo import shoujiinfo
-device=shoujiinfo.connnect_ipad_device()

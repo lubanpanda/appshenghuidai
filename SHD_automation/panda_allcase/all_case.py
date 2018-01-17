04d21f3f8b7540e9bbae4fc41b7dae40d16f04b6
panda <84305510@qq.com>
2018-01-17 10:07:10 +0800

    修改日期格式

--- SHD_automation/panda_allcase/all_case.py
+++ SHD_automation/panda_allcase/all_case.py
@@ -12,7 +12,7 @@ import time
 case_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # 报告存放路径
-report_path = os.path.join('/Users/yuchengtao/PycharmProjects/shenghuidai/SHD_automation/panda_baogao/')
+report_path = os.path.join('/Users/yuchengtao/PycharmProjects/shenghuidai/SHD_automation/panda_baogao/case_baogao')
 def all_case():
     discover = unittest.defaultTestLoader.discover(case_path,pattern="test*.py",top_level_dir=None)
     print(discover)
@@ -20,8 +20,7 @@ def all_case():
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
-    # runner.run(all_case())
-    curtime = time.strftime ('%Y%m%d%H%M%S', time.localtime ())
+    curtime = time.strftime ('%Y-%m-%d %H:%M:%S', time.localtime ())
     report_paths = report_path + curtime + '.html'
     report_set = open (report_paths, 'wb')
     runner = HTMLTestRunner.HTMLTestRunner (stream = report_set, title = '自动化测试报告', description = '胜辉贷各用例执行情况：')

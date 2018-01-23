8c209fff82348b5a764953725d26560dba875ac1
panda <84305510@qq.com>
2018-01-23 14:57:14 +0800

    增加变量的注释

--- SHD_automation/panda_methods/my_methods.py
+++ SHD_automation/panda_methods/my_methods.py
@@ -13,8 +13,14 @@ from SHD_automation.device_info.device import *
 
 # noinspection PyUnresolvedReferences,PyPep8Naming
 class My_method(object):
-	'获取ID元素'
 	def My_id(self,id,text,shuxing=None,sleep_time=0):
+		'''
+		:param id: 元素ID
+		:param text: 操作对象
+		:param shuxing: 获取text内容或其他控件信息
+		:param sleep_time: 睡眠时间
+		:return: 获取ID元素或者其他熟悉操作
+		'''
 		if text == '获取元素':
 			pro = '获取元素>>>：'
 			logging.info (u'%s%s' % (pro, id))
@@ -36,14 +42,25 @@ class My_method(object):
 				pro = '输入内容为>>>：'
 				logging.info (u'定位控件%s,%s%s' % (id, pro, text))
 				return self.driver.find_element_by_id (id).clear(),self.driver.find_element_by_id (id).send_keys (str(text)),time.sleep(sleep_time)
-	'''通过accessibility id查找元素'''
+
 	def my_accessibility_id_dianji(self,accessibility_id,sleep_time=0):
+		"""
+		:param accessibility_id:
+		:param sleep_time:
+		:return: 通过accessibility id查找元素
+		"""
 		proo='accessibility id查找元素>>>'
 		logging.info(f'{proo}{accessibility_id}')
 		return self.device.find_element_by_accessibility_id(accessibility_id).click(),time.sleep(sleep_time)
 
-	'''方法包装_通过当前页面:classname+text定位控件并完成输入'''
+
 	def my_class_name_shuru (self,className, text, txtUsername):
+		"""
+		:param className: 元素
+		:param text: text内容
+		:param txtUsername: 输入内容
+		:return: 方法包装_通过当前页面:classname+text定位控件并完成输入
+		"""
 		allClassNames = self.driver.find_elements_by_class_name (className)  # 定义所有该className下所有控件为 allclassname
 		for allClassName in allClassNames:
 			print (allClassName.text)
@@ -51,9 +68,16 @@ class My_method(object):
 				allClassName.set_text (txtUsername)
 				break
 
-	'''封装一个根据clsaa+id的方法点击控件'''
-	def my_class_name_id_dianji(self,classname,list_id,text,shuxing=None,sleep_time=1):
 
+	def my_class_name_id_dianji(self,classname,list_id,text,shuxing=None,sleep_time=1):
+		"""
+		:param classname: class名字
+		:param list_id: 索引的数值
+		:param text: 自己输入的操作方法
+		:param shuxing: 查看text或其他参数可用
+		:param sleep_time: 默认的睡眠时间
+		:return: 封装了获取元素，点击操作，输入内容，获取控件单位方法
+		"""
 		if text == '获取元素':
 			proo = '获取classname元素>>>：'
 			logging.info (u'%s%s' % (proo, classname))
@@ -75,16 +99,26 @@ class My_method(object):
 				pros = '输入内容为>>>：'
 				logging.info (u'定位控件%s,%s%s' % (classname, pros, text))
 				return self.driver.find_element_by_id (classname).set_text (text)
-	'''封装一个根据clsaa+text的方法点击控件 '''
+
 	def my_class_name_shuru_dianji (self, className, text):
+		"""
+		:param className: class元素
+		:param text: text控件名称
+		:return: 封装一个根据clsaa+text的方法点击控件
+		"""
 		clickClassName = self.driver.find_elements_by_class_name (className)
 		for clickclassNameOne in clickClassName:
 			if text in clickclassNameOne.text:
 				clickclassNameOne.click ()
 				break
 
-	'''封装一个根据class+text的方法获取元素'''
+
 	def my_classText_huoqu (self, className, text):
+		"""
+		:param className: 名字
+		:param text: text想找的内容
+		:return: 封装一个根据class+text的方法获取元素
+		"""
 		clickClassName = self.driver.find_elements_by_class_name (className)
 		for clickclassNameOne in clickClassName:
 			if text in clickclassNameOne.text:
@@ -93,35 +127,55 @@ class My_method(object):
 				logging.info (text)
 				break
 			else:
-				logging.info ('sjoj')
+				logging.info (f'没找到{text}')
 
-	'''封装一个获取className的方法（className唯一）'''
+
 	def huoQu_className (self, className):
+		"""
+		:param className: class元素名称
+		:return: 封装一个获取className的方法（className唯一）
+		"""
 
 		return self.driver.find_element_by_class_name (className)
 
-	'''封装一个滑动当前页面查找元素方法'''
-	def scroll_resourceId (self,classNameCode, textCode):
+
+	def scroll_resourceId (self, idcode, textCode):
+		"""
+		:param idcode: id名字
+		:param textCode: 要寻找的名字
+		:return: 封装一个滑动当前页面查找元素方法
+		"""
 		self.driver.find_element_by_android_uiautomator (
 			'new UiScrollable(new UiSelector().resourceId("%s")).scrollIntoView(new UiSelector().text("%s"))' % (
-				classNameCode, textCode))
+				idcode, textCode))
 
-	'''封装一个滑动当前页面class+text查看元素方法'''
 
 	def scroll_classText (self, classNameCode, textCode):
+		"""
+		:param classNameCode: class名字
+		:param textCode: 要寻找的名字
+		:return: 封装一个滑动当前页面class+text查看元素方法
+		"""
 		self.driver.find_element_by_android_uiautomator (('new UiScrollable(new UiSelector().className("%s")).scrollIntoView(new UiSelector().text("%s"))' % (classNameCode, textCode)))
 
-	'''封装退出APP方法'''
+
 
 	def back_Login (self):
+		"""
+		:return: 封装退出APP方法
+		"""
 		time.sleep (10)
 		self.driver.find_element_by_id (module_info['账户']).click ()
 		os.popen ('adb shell input swipe 50 1000 50 0 100')
 		self.driver.find_element_by_id (account['更多']).click ()
 		self.driver.find_element_by_id (module_info['退出']).click ()
 		self.driver.find_element_by_id ('com.yourenkeji.shenghuidai:id/positiveButton').click ()
-	'''封装获取toast方法'''
+
 	def find_toast (self,message):
+		"""
+		:param message: 吐司的信息内容
+		:return: 封装获取toast方法
+		"""
 		toast_Code = ('xpath', './/*[contains(@text,"%s")]' % message)
 		t = (WebDriverWait (self.driver, 5,0.1).until (EC.presence_of_element_located (toast_Code))).text
 		if message==t:
@@ -131,9 +185,12 @@ class My_method(object):
 			logging.info(f'吐司错误，正确的吐司是toast :{t}')
 			return t
 
-	'''封装登陆方法'''
 	def loginCode (self,username,password):
-		# 登录
+		"""
+		:param username: 账户
+		:param password: 密码
+		:return: 封装登陆方法
+		"""
 		My_method.My_id (self, module_info ['账户'], 'click')
 		My_method.My_id (self, zhuce ['登录'], 'click')
 		My_method.My_id (self, zhuce ['账号'], username)
@@ -142,8 +199,13 @@ class My_method(object):
 		My_method.My_id (self, 'com.yourenkeji.shenghuidai:id/denglu_bt_a', 'click')
 		My_method.My_id (self, zhuce ['跳过'], 'click')
 		logging.info ('初始化登陆成功')
-	'''非正常退出的登录方式'''
+
 	def yiwai_login(self,uesrnames=15201525754,passwords=123456):
+		"""
+		:param uesrnames: 账户
+		:param passwords: 密码
+		:return: token失效时调用这个函数
+		"""
 		My_method.my_class_name_id_dianji (self, 'android.widget.Button', 0, 'click')
 		My_method.My_id (self, zhuce ['账号'], uesrnames)
 		My_method.My_id (self, 'com.yourenkeji.shenghuidai:id/zhu_bt', 'click')
@@ -151,16 +213,25 @@ class My_method(object):
 		My_method.My_id (self, 'com.yourenkeji.shenghuidai:id/denglu_bt_a', 'click')
 		My_method.My_id (self, zhuce ['跳过'], 'click')
 		logging.info('重新登录成功')
-	'''封装一个退出方法'''
+
 	def login_exit(self):
+		"""
+		:return: 封装一个退出方法
+		"""
 		My_method.My_id(self,module_info['账户'],'click')
 		My_method.My_id(self,account['更多'],'click')
 		My_method.My_id(self,module_info['退出'],'click')
 		My_method.My_id(self,'com.yourenkeji.shenghuidai:id/positiveButton','click')
 		logging.info('退出账号成功')
 
-	'''判断是否登录'''
+
 	def login_turn_or_flase(self,panduan_login,name,password):
+		"""
+		:param panduan_login: 提供一个执行条件
+		:param name: 账号名字
+		:param password: 账号密码
+		:return: 判断是否登录
+		"""
 		My_method.My_id (self, module_info ['账户'], 'click')
 		if panduan_login=='判断登录':
 			self.driver.implicitly_wait (3)
@@ -172,8 +243,11 @@ class My_method(object):
 			except Exception:
 				logging.info('已经登录了哦')
 
-	'''封装一个多次返回的方法'''
 	def app_back(self,cishu=1):
+		"""
+		:param cishu: 返回的次数，默认为1
+		:return: 封装一个多次返回的方法
+		"""
 		for i in range(cishu):
 			self.driver.back()
 		pro = '一共返回了:'
@@ -181,9 +255,10 @@ class My_method(object):
 
 # noinspection PyUnresolvedReferences
 class myMethod (object):
-	'''封装一个随机生成电话号码的方法,默认方法首位字母为1，其余十位随机'''
-
 	def randomTel (self):
+		"""
+		:return: 封装一个随机生成电话号码的方法,默认方法首位字母为185，其余八位随机
+		"""
 		i = 1
 		listUsername = ['185']
 		while i <= 8:
@@ -193,8 +268,13 @@ class myMethod (object):
 		logging.info(f'手机号为:{listUsername}')
 		return ''.join (listUsername)
 
-	'''等待定位元素'''
+
 	def wait_time (self, resourceid, waitTime = None):
+		"""
+		:param resourceid: 需要查找的ID
+		:param waitTime: 等待时间
+		:return: 等待定位元素
+		"""
 		try:
 			if waitTime == None:
 				waitTime = 10
@@ -204,11 +284,13 @@ class myMethod (object):
 			print (f)
 			logging.info (u'>>>未检测到{},页面跳转成功'.format (resourceid))
 
-'''封装一个解锁手机功能'''
 
 # noinspection PyUnresolvedReferences
 class Pingmu_unlock_the_screen(object):
 	def pingmu_jiesuo(self):
+		"""
+		:return: 手机本身的解锁
+		"""
 		b = os.popen ('adb shell dumpsys window policy|grep mScreenOnFully')
 		a = b.read ().strip ()
 		deng = a [-5:]
@@ -223,6 +305,9 @@ class Pingmu_unlock_the_screen(object):
 			logging.info('不是锁屏状态,可直接执行项目哦')
 
 	def App_jiesuo(self):
+		"""
+		:return: 解锁app内部的滑动解锁
+		"""
 		time.sleep(6)
 		list_pwd = self.driver.find_elements_by_class_name ("android.widget.ImageView")
 		logging.info(list_pwd)
@@ -235,16 +320,24 @@ class Pingmu_unlock_the_screen(object):
 		except Exception:
 			logging.info('解锁失败')
 
-'''封装一个滑动的方法'''
 
 # noinspection PyUnresolvedReferences
 class Huadong(object):
 	def Getsize (self):
+		"""
+		:return:获取手机的分辨率
+		"""
 		x = self.driver.get_window_size () ['width']
 		y = self.driver.get_window_size () ['height']
 		logging.info(x,y)
 		return x, y
 	def huadong(self,fangxiang,huadong_time,sleep_time=1):
+		"""
+		:param fangxiang: 滑动的方向
+		:param huadong_time: 滑动时间
+		:param sleep_time: 睡眠时间
+		:return: 滑动的方法
+		"""
 		screen_size = Huadong.Getsize (self)
 		if fangxiang=='上':
 			x1=int(screen_size[0]*0.5)
@@ -276,16 +369,24 @@ class Huadong(object):
 			return self.driver.swipe (x5, y6, x6, y6, huadong_time),time.sleep(sleep_time)
 		else:
 			logging.info("写错了哦，无法滑动")
-'''截图功能'''
+
 class jietu(object):
 	def jietu_picture(self,name):
+		"""
+		:param name: 相片的名字
+		:return: 截图功能
+		"""
 		self.driver.implicitly_wait (1)
 		picture_time=time.strftime ('%Y-%m-%d %H:%M:%S', time.localtime ())
 		self.driver.save_screenshot((HTMLbaogao['图片地址']+picture_time+"%s.png")%name)
 
-'''倒计时功能来阻止程序运行'''
+
 class daojishi(object):
 	def daojishi_seconds(self,seconds):
+		"""
+		:param seconds: 倒计时的秒数
+		:return: 倒计时功能来阻止程序运行
+		"""
 		count = 0
 		while count < seconds:
 			ncount = seconds - count
@@ -293,6 +394,3 @@ class daojishi(object):
 			sys.stdout.flush ()
 			time.sleep (1)
 			count += 1
-if __name__ == '__main__':
-	a=daojishi()
-	a.daojishi_seconds(5)

02615c7ba307fe3997be2f2b5109fedf4e01b07c
panda <84305510@qq.com>
2018-01-24 13:38:08 +0800

    增加函数说明

--- SHD_automation/panda_allcase/all_case.py
+++ SHD_automation/panda_allcase/all_case.py
@@ -15,7 +15,10 @@ case_path = os.path.join('../panda_case')
 # 报告存放路径
 report_path = os.path.join('../panda_baogao/')
 
-def all_case():
+def All_case():
+    """
+    :return: 执行所有以test开头的.py文件
+    """
     discover = unittest.defaultTestLoader.discover(case_path,pattern="test*.py",top_level_dir=None)
     return discover
 
@@ -28,5 +31,5 @@ if __name__ == "__main__":
     baogao_info = '内容包括：\n 一.首页\n1.首页的投资攻略模块\n2.新手指引模块\n3.每日签到模块\n4.邀请好友模块带完成\n5.帮助中心\n二.发现模块\n1.平台数据\n2.安全保障\n3.积分商城\n4.活动中心\n三.账户模块\n1.除【提现，充值】外的所以功能'
     logging.info(f"报告详情：{baogao_info}")
     runner = HTMLTestRunner.HTMLTestRunner (stream = report_set, title = '自动化测试报告', description = baogao_info)
-    runner.run (all_case ())
+    runner.run (All_case())
     report_set.close ()

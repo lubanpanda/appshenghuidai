062d17a67a606b33da9e82224df5a2c2fd2c1af9
panda <84305510@qq.com>
2018-02-08 17:34:54 +0800

    优化地址

--- SHD_automation/panda_allcase/all_case.py
+++ SHD_automation/panda_allcase/all_case.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     report_paths = report_path + curtime + '.html'
     report_set = open (report_paths, 'wb')
     # noinspection LongLine
-    baogao_info = '内容包括：\n 一.首页\n1.首页的投资攻略模块\n2.新手指引模块\n3.每日签到模块\n4.邀请好友模块带完成\n5.帮助中心\n二.发现模块\n1.平台数据\n2.安全保障\n3.积分商城\n4.活动中心\n三.账户模块\n1.除【提现，充值】外的所以功能'
+    baogao_info = '内容包括:一.首页1.首页的投资攻略模块2.新手指引模块3.每日签到模块4.邀请好友模块带完成5.帮助中心二.发现模块1.平台数据2.安全保障3.积分商城4.活动中心三.账户模块1.除【提现，充值】外的所以功能'
     logging.info(f"报告详情：{baogao_info}")
     runner = HTMLTestRunner.HTMLTestRunner (stream = report_set, title = '自动化测试报告', description = baogao_info)
     runner.run (All_case())

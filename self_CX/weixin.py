0604d4724aa8aa71da83a38bb5d04899bbdd2057
panda <84305510@qq.com>
2018-02-17 20:11:25 +0800

    添加一些注释

--- self_CX/weixin.py
+++ self_CX/weixin.py
@@ -22,7 +22,7 @@ class weixin(object):
 		self.friends = itchat.get_friends (update = True) [0:]
 		self.myUserName=itchat.get_friends(update=True)[0]["UserName"]
 		self.friedd_data=[]
-		self.book = xlwt.Workbook (encoding = 'utf-8', style_compression = 0)#style_compression是否进行文件压缩，0代表否
+		self.book = xlwt.Workbook (encoding = 'utf-8', style_compression = 0)   #style_compression是否进行文件压缩，0代表否
 		self.sheet = self.book.add_sheet ('微信详细信息', cell_overwrite_ok = True)
 		self.name = time.strftime ("%Y-%m-%d %H:%M:%S", time.localtime ())
 
@@ -30,14 +30,14 @@ class weixin(object):
 	def weixin_info(self):
 		for friend in self.friends[1:]:
 			f_d={
-				'uuid':friend['UserName'],#账号
-				'niceName':friend ['NickName'],#网民
-				'remarkName' : friend ['RemarkName'],#备注名
-				'city' : friend ['City'],#地点
-				'sex' : friend ['Sex'],#性别1为男
-				'province' : friend ['Province'],#省份
-				'signature' : friend ['Signature']#签名
-			}
+				'uuid':friend['UserName'],                      #账号
+				'niceName':friend ['NickName'],                 #网民
+				'remarkName' : friend ['RemarkName'],           #备注名
+				'city' : friend ['City'],                       #地点
+				'sex' : friend ['Sex'],                         #性别1为男
+				'province' : friend ['Province'],               #省份
+				'signature' : friend ['Signature']              #签名
+			}                                                   #获取你需要的数据内容
 			self.friedd_data.append(f_d)
 		feiend_number=len(self.friedd_data)
 		boy=0
@@ -47,13 +47,13 @@ class weixin(object):
 			beizhu=self.friedd_data[i]['remarkName']
 			id=self.friedd_data[i]['uuid']
 			Sex=self.friedd_data[i]['sex']
-			if self.friedd_data[i]['sex']==1:
+			if self.friedd_data[i]['sex']==1:                   #定义性别，把数字转化为男或女
 				xingbie='男'
 			else:
 				xingbie='女'
-			if not beizhu:
-				self.sheet.write(i+1,0,self.friedd_data[i]['niceName'])
-				print(self.friedd_data[i]['niceName']+',性别：'+xingbie+',uuid='+self.friedd_data[i]['uuid'])#网名
+			if not beizhu:  #判断是否有备注
+				self.sheet.write(i+1,0,self.friedd_data[i]['niceName'])     #向excel表格写数据
+				print(self.friedd_data[i]['niceName']+',性别：'+xingbie+',uuid='+self.friedd_data[i]['uuid'])  #网名
 				if xingbie=='男':
 					boy += 1
 				else:
@@ -66,20 +66,14 @@ class weixin(object):
 				else:
 					girl+=1
 
-			shuxing_info=[self.friedd_data[i]['niceName'],xingbie,self.friedd_data[i]['uuid'],self.friedd_data[i]['city'],self.friedd_data[i]['signature'],self.friedd_data[i]['province']]
+			shuxing_info=[self.friedd_data[i]['niceName'],xingbie,self.friedd_data[i]['uuid'],self.friedd_data[i]['city'],self.friedd_data[i]['signature'],self.friedd_data[i]['province']]         #定义个excel输入的详细信息属性
 			for a in range(6):
 				self.sheet.write(i+1,1+a,shuxing_info[a])
 		head = ['备注', '网名', '性别', '微信ID', '城市', '个性签名', '省份']
 		for b in range (7):
 			self.sheet.write (0, b, head [b])
-		self.book.save ('../self_CX/'+self.name+'.xls')
+		self.book.save ('../self_CX/'+self.name+'.xls') #保存文件
 		print(f'一共有{feiend_number}位好友，男孩子有{boy}个，女孩子有{girl}个')
-		if boy>girl:
-			print('你的微信怎么全是男孩子哦，要添加点异性朋友哦')
-		elif boy-girl==0:
-			print('男女朋友刚刚好，very good')
-		else:
-			print('额。。。。。女同志有点多哦')
 
 #发送消息
 	def send_weixin(self,name,message):
@@ -87,17 +81,10 @@ class weixin(object):
 		:param message: 发送消息的内容
 		:return:
 		"""
-		itchat.search_friends ()
-		users = itchat.search_friends (name = name)  # 使用备注名来查找实际用户名
+		users = itchat.search_friends (name = name)                         # 使用备注名来返回一个字典
 		userName = users [0] ['UserName']
 		print(f'名字：{userName}')
-		i=1
-		while True:
-			itchat.send(msg =message,toUserName = userName)
-			i += 1
-			print(i)
-			time.sleep(0.2)
-			continue
+		itchat.send(msg =message,toUserName = userName)
 
 #自动回复消息
 @itchat.msg_register('Text')
@@ -133,7 +120,7 @@ def personal_msg (msg):
 		'Type'] == 'Recording':
 		print(msg_time_rec + "  " + msg_from + ' send a ' + msg ['Type'])
 
-# 接收群聊天信息，如果不需要可以不运行
+# 接收群聊天信息
 @itchat.msg_register ([TEXT, PICTURE, FRIENDS, CARD, MAP, SHARING, RECORDING, ATTACHMENT, VIDEO],
                       isFriendChat = False, isGroupChat = True, isMpChat = False)
 def chatroom_msg (msg):
@@ -159,7 +146,8 @@ def chatroom_msg (msg):
 
 if __name__ == '__main__':
 	a=weixin()
-	a.weixin_info()
+	# a.weixin_info()
+	a.send_weixin('夏天','你好')
 	# a.chatroom_msg()
 	# itchat.auto_login (hotReload = True)
 	# itchat.run ()

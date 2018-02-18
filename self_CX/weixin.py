64a46a848cf55da7f7fcb089326dcb2db69d31c0
panda <84305510@qq.com>
2018-02-18 14:24:08 +0800

    添加一些注释

--- self_CX/weixin.py
+++ self_CX/weixin.py
@@ -51,7 +51,7 @@ class weixin(object):
 				xingbie='男'
 			else:
 				xingbie='女'
-			if not beizhu:  #判断是否有备注
+			if not beizhu:                                      #判断是否有备注
 				self.sheet.write(i+1,0,self.friedd_data[i]['niceName'])     #向excel表格写数据
 				print(self.friedd_data[i]['niceName']+',性别：'+xingbie+',uuid='+self.friedd_data[i]['uuid'])  #网名
 				if xingbie=='男':
@@ -66,13 +66,13 @@ class weixin(object):
 				else:
 					girl+=1
 
-			shuxing_info=[self.friedd_data[i]['niceName'],xingbie,self.friedd_data[i]['uuid'],self.friedd_data[i]['city'],self.friedd_data[i]['signature'],self.friedd_data[i]['province']]         #定义个excel输入的详细信息属性
+			shuxing_info=[self.friedd_data[i]['niceName'],xingbie,self.friedd_data[i]['uuid'],self.friedd_data[i]['city'],self.friedd_data[i]['signature'],self.friedd_data[i]['province']]               #定义个excel输入的详细信息属性
 			for a in range(6):
 				self.sheet.write(i+1,1+a,shuxing_info[a])
 		head = ['备注', '网名', '性别', '微信ID', '城市', '个性签名', '省份']
 		for b in range (7):
 			self.sheet.write (0, b, head [b])
-		self.book.save ('../self_CX/'+self.name+'.xls') #保存文件
+		self.book.save ('../self_CX/'+self.name+'.xls')          #保存文件
 		print(f'一共有{feiend_number}位好友，男孩子有{boy}个，女孩子有{girl}个')
 
 #发送消息
@@ -81,7 +81,7 @@ class weixin(object):
 		:param message: 发送消息的内容
 		:return:
 		"""
-		users = itchat.search_friends (name = name)                         # 使用备注名来返回一个字典
+		users = itchat.search_friends (name = name)               # 使用备注名来返回一个字典
 		userName = users [0] ['UserName']
 		print(f'名字：{userName}')
 		itchat.send(msg =message,toUserName = userName)

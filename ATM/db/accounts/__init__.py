4b910f96ec6afe1d25559cb81f5693e59c01f9ce
panda <84305510@qq.com>
2018-07-10 21:43:03 +0800

    ......

--- ATM/db/accounts/__init__.py
+++ ATM/db/accounts/__init__.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @Author  : panda  
-import os
-
-for (dirpath, dirnames, filenames) in os.walk ('./'):  # 第一个起始路径，第二个为起始路径下的文件夹，第三个是起始路径下的文件。
-	for file in filenames:
-		print(file)
+# @Author  : panda

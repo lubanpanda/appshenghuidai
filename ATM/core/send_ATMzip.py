e785e5c5e61bfbb6039d6e4a485ba88c5310c27e
panda <84305510@qq.com>
2018-08-13 21:30:08 +0800

    测试jeent

--- ATM/core/send_ATMzip.py
+++ ATM/core/send_ATMzip.py
@@ -33,14 +33,14 @@ def Send_QQ_Email (name):
 	username = '1007596772@qq.com'
 	password = 'dpkbrcajfhsibfgi'  # '填写自己的QQ号的授权码'
 	sender = username
-	receivers = []
-	while True:
-		send_qq = input ('请输入要发送的QQ号码,按回车输入下一位QQ接受人，结束时直接按回车即可完成发送' + os.linesep).strip ()
-		if send_qq is not None and send_qq != '':
-			receivers.append (send_qq + '@qq.com')
-			continue
-		else:
-			break
+	receivers = ['84305510@qq.com']
+	# while True:
+	# 	send_qq = input ('请输入要发送的QQ号码,按回车输入下一位QQ接受人，结束时直接按回车即可完成发送' + os.linesep).strip ()
+	# 	if send_qq is not None and send_qq != '':
+	# 		receivers.append (send_qq + '@qq.com')
+	# 		continue
+	# 	else:
+	# 		break
 	msg = MIMEMultipart ()
 	msg ['Subject'] = 'ATM小程序'  # 标题
 	msg ['From'] = sender
@@ -64,3 +64,8 @@ def Send_QQ_Email (name):
 		print ('邮件发送成功！', receivers, name)
 	except Exception as e:
 		print ('邮件发送失败', e)
+
+
+if __name__ == '__main__':
+	Make_zip (BASE_DIR, BASE_DIR + '.zip')
+	Send_QQ_Email (BASE_DIR + '.zip')

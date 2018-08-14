312dce6a19742b6a4b627f16c5a38129506d0713
panda <84305510@qq.com>
2018-08-14 21:30:25 +0800

    再试试集成

--- ATM/core/send_ATMzip.py
+++ ATM/core/send_ATMzip.py
@@ -32,13 +32,6 @@ def Send_QQ_Email (name):
 	password = 'dpkbrcajfhsibfgi'  # '填写自己的QQ号的授权码'
 	sender = username
 	receivers = ['84305510@qq.com']
-	# while True:
-	# 	send_qq = input ('请输入要发送的QQ号码,按回车输入下一位QQ接受人，结束时直接按回车即可完成发送' + os.linesep).strip ()
-	# 	if send_qq is not None and send_qq != '':
-	# 		receivers.append (send_qq + '@qq.com')
-	# 		continue
-	# 	else:
-	# 		break
 	msg = MIMEMultipart ()
 	msg ['Subject'] = 'ATM小程序'  # 标题
 	msg ['From'] = sender

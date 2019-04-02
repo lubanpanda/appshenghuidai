57621cf0933313c3c91747d47a942add71e7aae5
panda <84305510@qq.com>
2019-04-02 16:11:15 +0800

    启动jemter

---
+++ self_CX/Others/start_jemter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "panda  84305510@qq.com"
+
+import os
+
+
+def start_jmeter():
+    os.popen('/Applications/apache-jmeter-5.0/bin/jmeter ; exit;')
+
+
+if __name__ == '__main__':
+    start_jmeter()

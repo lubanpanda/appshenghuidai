83b7fb023a858c0ed4a10a5118ce79e161135357
panda <84305510@qq.com>
2019-06-22 15:22:06 +0800

    自动更新所有的pip包的库

---
+++ self_CX/Others/pipupdate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "panda  84305510@qq.com"
+
+from subprocess import call
+
+from pip._internal.utils.misc import get_installed_distributions
+
+for dist in get_installed_distributions():
+    call("pip3 install --upgrade " + dist.project_name, shell=True)

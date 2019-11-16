e1661fb93afc2e913a2b76745d7ae5d2930dbe35
panda <84305510@qq.com>
2019-11-16 18:14:25 +0800

    添加主方法

--- self_CX/Others/pipupdate.py
+++ self_CX/Others/pipupdate.py
@@ -6,5 +6,11 @@ from subprocess import call
 
 from pip._internal.utils.misc import get_installed_distributions
 
-for dist in get_installed_distributions():
-    call("pip3 install --upgrade " + dist.project_name, shell=True)
+
+def pythonpacgeUpdate():
+    for dist in get_installed_distributions():
+        call("pip3 install --upgrade " + dist.project_name, shell=True)
+
+
+if __name__ == '__main__':
+    pythonpacgeUpdate()

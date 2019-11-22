7eb4d88c3d21e8e25a8817f7f7d17af863cb65c4
panda <84305510@qq.com>
2019-11-22 10:29:54 +0800

    更多日志的记录

--- self_CX/Others/testlog.py
+++ self_CX/Others/testlog.py
@@ -20,6 +20,46 @@ def Loggings():
     return logging
 
 
+##################################################################
+
+from loguru import logger
+
+logger.add("aa.log", encoding='utf-8')
+logger.debug('这是一个bug')
+logger.info("this is a info message")
+logger.error("this is a error message")
+logger.warning("this is a warning message")
+
+import socket
+
+hostname = socket.gethostname()
+name = socket.gethostbyname(hostname)
+print(name)
+input('按任意键退出')
+
+from logzero import logger
+
+# import uiautomation
+# These log messages are sent to the console
+logger.debug("这是一个程序")
+logger.info("读取了一条数据")
+logger.warning("此数据可能有异常")
+logger.error("保存按钮没有找到")
+from logbook import Logger, StreamHandler, TimedRotatingFileHandler
+import sys
+import os
+
+StreamHandler(sys.stdout).push_application()
+log = Logger('Logbook')
+log.info('Hello, World!')
+LOG_DIR = os.path.join('log')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+TimedRotatingFileHandler(os.path.join(LOG_DIR, '%s.log' % 'user_log'), date_format='%Y%m%d',
+                         bubble=True).push_application()
+user_log = Logger('user_log')
+user_log.info('user_log mytest....')
+
 
 
 # initLogging('a.log')
